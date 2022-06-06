7488355041f421bea9b84eed4c39663b4b46c8a9
Nihal Vishal Gupta <nihalng786@gmail.com>
2022-06-07 00:14:41 +0530

    Optimal Storage added

---
+++ greedy/optimalStorage.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int n ;
+
+void optimal(int l[] , int n ){
+
+    int i , j , sum ;
+
+    sort(l , l+n);
+
+    cout<<"The optimal sequence to store tapes is :";
+    for(int i = 0 ; i < n ; i++)
+    cout<<l[i]<<" ";
+    cout<<endl ; 
+
+    double MRT = 0;
+    for(i = 0 ; i<n ; i++){
+        sum = 0 ;
+    for(j  = 0 ; j < i ; j++){
+        sum += l[i];
+    }
+    MRT += sum ;
+    }
+
+    MRT /= n ;
+
+    cout<<"The MRT is :";
+    cout<<MRT;
+}
+
+int main(){
+
+    cout<<"Enter  the number of tapes :";
+    cin>>n;
+    
+
+    int l[100];
+    int i ;
+
+    cout<<"\nEnter the elements of the tape :";
+    for(i=0 ; i<n ; i++){
+        cin>> l[i];
+    }
+
+    optimal(l , n);
+    return 0;
+
+}
